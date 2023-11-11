a1e3988776c808d822c695e8dd5378a11a5d50ce
Matheus Andrade <matheusla2f04@gmail.com>
2023-11-11 12:42:50 +0000

    att

--- src/app/studentOpportunity/page.tsx
+++ src/app/studentOpportunity/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { BsFillCheckCircleFill, BsPlusCircleFill } from 'react-icons/bs'
+import { BsChevronDown, BsPlusCircleFill } from 'react-icons/bs'
 import HeaderStudent from "@/components/headerStudent"
 
 export default function studentOpportunity() {
@@ -16,13 +16,19 @@ export default function studentOpportunity() {
             </div>
             <div className="flex justify-between flex-col px-20 py-12 bg-grey-fg">
                 <div>
-                    <p className="py-2 text-red-fg font-bold">ENFERMEIRO SOCORRISTA - HOSPITAL SANTA CASA</p>
+                    <div className="flex justify-between">
+                        <p className="py-2 text-red-fg font-bold">ENFERMEIRO SOCORRISTA - HOSPITAL SANTA CASA</p>
+                        <BsChevronDown />
+
+                    </div>
                     <p className="py-2 font-semibold">Empresa: Hospital Santa Casa de Misericordia, 23455675334/21</p>
                     <p className="py-2 font-semibold">Local: Rua Verde, 234, Recife, Joana Bezerra</p>
                     <p className="py-2"><span className="font-semibold">Descrição:</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae fermentum massa. Aliquam tincidunt, metus non ultrices vehicula, libero lacus faucibus quam, in tristique lectus dolor nec est. Nulla tempus sem id erat dignissim euismod. Nunc tincidunt blandit pharetra. Cras fringilla gravida lorem ac semper. Phasellus tempus mauris sed nunc ultrices, ac porta urna commodo.</p>
                 </div>
-                <div className="py-6 right-0">
-                    <button className="flex items-center justify-center px-12 py-2 border bg-red-fg text-white font-semibold rounded-xl"><BsPlusCircleFill /> Candidatar-se</button>
+
+                <div className="py-6 flex justify-between">
+                    <p></p>
+                    <button className="flex items-center  px-12 py-2 border bg-red-fg text-white font-semibold rounded-xl"><BsPlusCircleFill/> Candidatar-se</button>
                 </div>
             </div>
         </main>
