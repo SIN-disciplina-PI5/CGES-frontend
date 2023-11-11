3d9b37682202cae4e80bb073817c4e6286b93edd
Beatriz <bia906@gmail.com>
2023-11-11 07:50:45 -0300

    Table Component

--- src/components/DocumentFormComponents/Table.tsx
+++ src/components/DocumentFormComponents/Table.tsx
@@ -1,25 +1,31 @@
+import Button from "../button";
 import FormInput from "./formInput";
 
 const Table = () => {
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg mb-20">
-      <table className="w-full text-sm text-left rtl:text-right text-black dark:text-black">
-        <thead className="text-xs text-white uppercase bg-gray-50 bg-red-700">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Principais Atividades
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Realizadas?
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <Atividade />
-          <Atividade />
-          <Atividade />
-        </tbody>
-      </table>
+    <div className="mb-20">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left rtl:text-right text-black dark:text-black">
+          <thead className="text-xs text-white uppercase bg-gray-50 bg-red-700">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Principais Atividades
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Realizadas?
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <Atividade />
+            <Atividade />
+            <Atividade />
+          </tbody>
+        </table>
+      </div>
+      <div className="w-full flex justify-center mb-10 mt-10">
+        <Button nav="#" title="Adicionar" width="w-56" height="h-16" />
+      </div>
     </div>
   );
 };
@@ -32,16 +38,16 @@ const Atividade = () => {
           scope="row"
           className="px-6 py-4 font-medium text-black whitespace-nowrap dark:text-black"
         >
-          <FormInput title="Atividade" />
+          <p>Atividade</p>
         </th>
-        <td className="px-6 py-10 flex justify-center items-center ">
+        <td className="px-6 py-10 flex flex-col justify-center items-center ">
           <label className="px-6">
-            Sim
             <input type="radio" name="resposta" id="" />
+            Sim
           </label>
           <label>
-            Não
             <input type="radio" name="resposta" id="" />
+            Não
           </label>
         </td>
       </tr>
